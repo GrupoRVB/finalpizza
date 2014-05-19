2939713a34344d01b9c8e0906967ed3c80c749b1
vvvictorgm <victorfrazatto@hotmail.com>
2014-05-19 14:57:00 -0300

    NAO SEI Q PORRA EH ESSA, MAS TA ME IMPEDINDO DE COMMITAR, SEJA O QUE SATÃ QUISER!

---
+++ Assets/Scripts/SomBotõesMenu.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SomBotõesMenu : MonoBehaviour {
+	private float lado;
+	private float limitador =0.0f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+		void Update () {
+				if (GetComponent<Menu> ().principal == true) {
+						lado = Input.GetAxis ("Horizontal");
+						if (lado > 0 && Time.time > limitador) {
+								limitador = Time.time + 0.3f;
+								audio.Play ();
+				
+				
+						}
+						if (lado < 0 && Time.time > limitador) {
+								limitador = Time.time + 0.3f;
+								audio.Play ();
+						}
+	
+				} else {
+				}
+		}
+}
+
