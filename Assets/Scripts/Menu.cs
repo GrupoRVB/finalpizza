bc24f03a70972e3aa5587be093cc4c32f08af928
vvvictorgm <victorfrazatto@hotmail.com>
2014-05-19 14:11:12 -0300

    menu ajustao

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -4,6 +4,7 @@ public class Menu : MonoBehaviour {
 	private Animator animator;
 	private float lado;
 	private int atual =1;
+	public bool principal =true;
 	private float limitador =0.0f;
 	public GameObject Controle;
 	public GameObject Creditos;
@@ -15,39 +16,66 @@ public class Menu : MonoBehaviour {
 	//teste
 	// Update is called once per frame
 	void Update () {
-				lado = Input.GetAxis ("Horizontal");
-				if (lado > 0 && Time.time > limitador) {
-						atual += 1;
-						if (atual > 4) {
-								atual = 1;
-						}
-						animator.SetInteger ("Selecionado", atual);
-						limitador = Time.time + 0.3f;
-
-
+		Debug.LogError (atual);
+		if (principal == true) {
+						Menu_inicial ();
+				} else {
+			Voltar();
 				}
-				if (lado < 0 && Time.time > limitador) {
-						atual -= 1;
-						if (atual < 1){
-								atual = 4;
+	
+	}
+	void Menu_inicial(){
+		lado = Input.GetAxis ("Horizontal");
+		if (lado > 0 && Time.time > limitador) {
+			atual += 1;
+			if (atual > 4) {
+				atual = 1;
 			}
-				animator.SetInteger ("Selecionado", atual);
-				limitador = Time.time + 0.3f;
-
+			animator.SetInteger ("Selecionado", atual);
+			limitador = Time.time + 0.3f;
+			
+			
+		}
+		if (lado < 0 && Time.time > limitador) {
+			atual -= 1;
+			if (atual < 1){
+				atual = 4;
+			}
+			animator.SetInteger ("Selecionado", atual);
+			limitador = Time.time + 0.3f;
+			
 		}
 		if(Input.GetButton("Selecionar")){
-			if(atual == 1){
-				Application.LoadLevel ("fase1");
-			}
-			if(atual ==2 ){
-			}
-			Controle.transform.Translate = new Vector2 (0.09841728f,0.0701046f)
-			if(atual == 3){
-			}
-			if(atual == 4){
-				Application.Quit();
-			}
+			Selecionar();
+			principal = false;
+		}
+	}
 
+	void Selecionar(){
+				if (Input.GetButton ("Selecionar")) {
+						if (atual == 1) {
+								Application.LoadLevel ("fase1");
+						}
+						if (atual == 2) {
+								Controle.transform.position = new Vector2 (0, 0);
+						}
+						if (atual == 3) {
+								Creditos.transform.position = new Vector2 (0, 0);
+						}
+						if (atual == 4) {
+								Application.Quit ();
+						}
+			
+				}
+		}
+	void Voltar(){
+		if (Input.GetButton ("Voltar") && atual == 2) {
+			Controle.transform.position = new Vector2 (0, 10);
+			principal = true;
+				}
+		if (Input.GetButton ("Voltar") && atual == 3) {
+			Creditos.transform.position = new Vector2 (0, 10);
+			principal = true;
 		}
 	}
 
