cf2111fc01ce66771d805c177ba53b3f66ab3b64
vvvictorgm <victorfrazatto@hotmail.com>
2014-05-31 20:02:26 -0300

    As coisas novas neh....pq agrega...

---
+++ Assets/Scripts/Global.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Global : MonoBehaviour {
+	public bool sound = true;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Som ();
+	}
+	void Som(){
+		if(Input.GetButtonDown ("Select")){
+			if(sound == true){
+				AudioListener.volume = 0.0f;
+				sound = false;
+			}else{
+				AudioListener.volume = 1.0f;
+				sound = true;
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/Som_menu.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Som_menu : MonoBehaviour {
+	public AudioClip mudança;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	public void Mudou(){
+		audio.PlayOneShot (mudança);
+	}
+}
