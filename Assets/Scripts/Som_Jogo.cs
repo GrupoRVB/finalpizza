b44d3101e9b110ed514cc86adfe92aa63aec7758
vvvictorgm <victorfrazatto@hotmail.com>
2014-06-28 00:37:20 -0300

    coisinhas

--- Assets/Scripts/Som_Jogo.cs
+++ Assets/Scripts/Som_Jogo.cs
@@ -3,28 +3,20 @@ using System.Collections;
 
 public class Som_Jogo : MonoBehaviour {
 	public AudioClip fase;
-	public AudioClip vitoria;
 	public GameObject player;
-	private bool morto;
-	public GameObject chefe;
+
+
 	// Use this for initialization
 	void Start () {
 		audio.clip = fase;
+
 
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		morto = chefe.GetComponent<Roboter_move> ().dead;
-		Debug.Log (chefe.GetComponent<Roboter_move> ().dead);
-		if (player.GetComponent<Movimento> ().chefe == true && morto == false ) {
+		if (player.GetComponent<Movimento> ().chefe == true) {
 			audio.volume -=0.021f* Time.deltaTime;
-		}
-		if (morto == true) {
-			audio.clip = vitoria;
-			audio.volume -=0.321f* Time.deltaTime;
-			audio.Play();
-
 		}
 
 	}
